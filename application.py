713d908ed47897aae733315d9d75a2d291ebae2b
Monish <monishkumar833@gmail.com>
2022-10-15 17:39:04 -0500

    can now display posts

--- application.py
+++ application.py
@@ -1,15 +1,18 @@
 from flask import Flask, render_template, request
 from pymongo import MongoClient
 
-client = MongoClient('mongodb+srv://edwoolbert:h0meisb0ss@cluster0.ejycszy.mongodb.net/?')
+client = MongoClient(
+    'mongodb+srv://edwoolbert:h0meisb0ss@cluster0.ejycszy.mongodb.net/?')
 db = client['tradewashu']
 users = db['users']
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     return render_template("index.html")
+
 
 @app.route("/login", methods=["POST"])
 def login():
@@ -18,14 +21,19 @@ def login():
 
     person = users.find_one({'username': username, 'password': password})
 
+    posts = [{'username': "mo", 'message': "mo", 'time': "mo"}, {'username': "mo", 'message': "mo", 'time': "mo"}, {'username': "mo",
+                                                                                                                    'message': "mo", 'time': "mo"}, {'username': "mo", 'message': "mo", 'time': "mo"}, {'username': "mo", 'message': "mo", 'time': "mo"}]
+
     if person:
-        return render_template("site.html", name = username, userId = person['id'])
+        return render_template("site.html", name=username, posts=posts)
 
     return render_template('error.html')
+
 
 @app.route("/signupPage", methods=["POST"])
 def signupPage():
     return render_template("signup.html")
+
 
 @app.route("/signup", methods=["POST"])
 def signup():
@@ -33,13 +41,16 @@ def signup():
     password = str(request.form.get("password"))
     email = str(request.form.get("email"))
 
-    users.insert_one({'id': [x for x in users.find()][-1]['id'] + 1, 'username': username, 'password': password, 'email': email})
+    users.insert_one({'id': [x for x in users.find()][-1]['id'] + 1,
+                     'username': username, 'password': password, 'email': email})
 
     return render_template("index.html")
+
 
 @app.route("/changePasswordPage", methods=["POST"])
 def chagePasswordPage():
     return render_template("changePassword.html")
+
 
 @app.route("/changePassword", methods=["POST"])
 def changePassword():
@@ -55,8 +66,9 @@ def changePassword():
         newvalues = {'$set': {'password': newPassword}}
         users.update_one(myquery, newvalues)
         return render_template('index.html')
-    
+
     return render_template('error.html')
+
 
 if __name__ == '__main__':
     app.run(use_debugger=False, use_reloader=False, passthrough_errors=True)
