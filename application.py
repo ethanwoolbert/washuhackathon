183037c38a5f8eb6a622a3657510c8fc5af16d48
Monish <monishkumar833@gmail.com>
2022-10-15 18:21:43 -0500

    added boostrap for posts

--- application.py
+++ application.py
@@ -21,8 +21,7 @@ def login():
 
     person = users.find_one({'username': username, 'password': password})
 
-    posts = [{'username': "mo", 'message': "mo", 'time': "mo"}, {'username': "mo", 'message': "mo", 'time': "mo"}, {'username': "mo",
-                                                                                                                    'message': "mo", 'time': "mo"}, {'username': "mo", 'message': "mo", 'time': "mo"}, {'username': "mo", 'message': "mo", 'time': "mo"}]
+    posts = [{'username': "mo", 'message': "Lorem ipsum dolor sit amet, adipiscing. Lorem ipsum dolor sit amet, consectetuer adipiscing. Lorem ipsum dolor sit amet, adipiscing. Lorem ipsum dolor sit amet, adipiscing. Lorem ipsum dolor sit amet, consectetuer adipiscing. Lorem ipsum dolor.", 'title': "mo", 'time': "mo"}]
 
     if person:
         return render_template("site.html", name=username, posts=posts)
